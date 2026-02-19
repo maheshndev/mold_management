37e7eddecd77bf32974bf64c4a85ce72cf7a29b0
maheshndev <maheshnarsale@outlook.com>
2026-02-19 15:31:43 +0530

    added patch for mould field in work order

--- mold_management/hooks.py
+++ mold_management/hooks.py
@@ -39,6 +39,7 @@ after_migrate = [
      "mold_management.patches.v0_1.add_is_mold_item_field_on_item.execute",
      "mold_management.patches.v0_1.add_other_than_mould_or_moulding_item.execute",
      "mold_management.patches.v0_1.add_is_moulding_item_checkbox_on_item.execute",
+     "aims_customization.patches.v_0.add_mould_in_workorder.execute"
     
 ]
 
